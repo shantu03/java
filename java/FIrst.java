fd12be009be97d2d2d3538e2a719320265a51278
shantu03 <hulwanshantanu@gmail.com>
2023-07-13 11:56:35 +0530

    on windows

---
+++ java/FIrst.java
@@ -0,0 +1,10 @@
+class First 
+{
+public static void main(String args[])
+{
+int a =9;
+int b;
+System.out.println(a+1);
+System.out.println(b);
+}
+}
---
+++ java/Second/Mesage.java
@@ -0,0 +1,11 @@
+
+public class Mesage {
+public static void main(String args[]) {
+	System.out.println("hello worlkhd ");
+	double b=5;
+	System.out.println(b);
+	char p ='a';
+	System.out.println();
+
+}
+}
