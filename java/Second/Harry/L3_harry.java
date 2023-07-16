e021e4c41a134448523db4e82c655685c4916517
shantu03 <hulwanshantanu@gmail.com>
2023-07-16 20:02:36 +0530

    harry code started

---
+++ java/Second/Harry/L3_harry.java
@@ -0,0 +1,17 @@
+package Harry;
+import P_Practice.first;		//Package import kel ahe
+public class L3_harry {
+	public static void main(String[] args)
+	{
+		
+		first on=new first();
+		on.hero();
+		
+		
+		
+		//JAR PACKAGE IMPORT NAHI KEL TR HI METHOD USE KARA
+//		FULLY QUALIFIED NAME METHOD :-
+		P_Practice.Second obj=new P_Practice.Second();
+		obj.show();
+	}
+}
---
+++ java/Second/P_Practice/Package_Practice.java
@@ -0,0 +1,71 @@
+package P_Practice;
+
+public class first{
+	 public void hero()
+	{
+		System.out.println("Practice Class --> hero()");
+	}
+
+}
+ public class Second{
+	public void show()
+	{
+		System.out.println("Second class  --> show()");
+	}
+}
+
+
+
+
+
+
+
+
+
+//import java.util.Scanner;
+//public class Practice{
+//	public static void main(String[] args) {
+//		Scanner sc=new Scanner(System.in);
+//		int k=sc.nextInt();
+//		int a;
+//		for(int i=1;i<=k;i++)
+//		{
+//			for(int j=i;j>=1;j--)
+//			{
+//				System.out.print(j +" ");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+//===============================================================
+//L16
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//
+//public class Practice{
+//	public static void main(String[] args) throws IOException {
+//		InputStreamReader tc=new InputStreamReader(System.in);
+//		BufferedReader sc=new BufferedReader(tc);
+//		String name=sc.readLine();
+//
+//		
+//		System.out.println(name);
+//	}
+//}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+//L16
+//import java.util.*;
+//public class Practice{
+//	public static void main(String[] args) {
+//		Scanner sh=new Scanner(System.in);
+//		System.out.println("enter two number for addtion ");
+//		int a=sh.nextInt();
+//		int b=sh.nextInt();
+//		System.out.println(a+b);
+//		
+//	}
+//}
