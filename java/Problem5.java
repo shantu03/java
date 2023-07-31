6f3fbb6daf3b15f372cf464b2627aeea8420fb91
shantu03 <hulwanshantanu@gmail.com>
2023-07-31 08:27:41 +0530

    first clean code

--- java/Problem5.java
+++ java/Problem5.java
@@ -1,4 +1,3 @@
-
 /*
 The Matrix class has methods for each of the following:
 1 - get the number of rows
@@ -16,7 +15,6 @@ class Matrix {
     private int rows;
     private int columns;
     public int[][] a;
-    int k=0;
 
 
     public void setRow(int row) {
@@ -69,7 +67,7 @@ class Matrix {
         }
         System.out.println();
     }
-    public void addtion(int row,int column,int a,int b)
+    public void addition(int row, int column, int a, int b)
     {
         this.a[row][column]=a+b;
     }
@@ -82,67 +80,107 @@ class Matrix {
 
 }
 
-public class Problem5{
-    public static void main(String[] args)
-    {
+public class Problem5 {
+    public static void main(String[] args) {
 
         String play;
+        String tab;
         do {
-            Matrix obj[]=new Matrix[3];
-            for(int i=0;i<3;i++)
-            {
-                obj[i]=new Matrix();
+            Matrix[] obj = new Matrix[3];
+            for (int i = 0; i < 3; i++) {
+                obj[i] = new Matrix();
                 obj[i].setRow(3);
                 obj[i].setColumn(4);
                 obj[i].arrayMemory();
-                if(i==2)
-                {
+                if (i == 2) {
                     continue;
                 }
                 obj[i].setArray();
             }
-            for(int i=0;i<2;i++)
-            {
-                obj[i].printArray(i+1);
-            }
 
-            while (true) {
-                System.out.println("Enter 1 for additon and 2 for multiplication");
-                int choice = new Scanner(System.in).nextInt();
-                if (choice == 1) {
-                    if ((obj[0].getRows() == obj[1].getRows()) && (obj[0].getColumns() == obj[1].getColumns())) {
-                        System.out.println("\nPERFORMING ADDTION : ");
-                        for (int i = 0; i < obj[0].getRows(); i++) {
-                            for (int j = 0; j < obj[0].getColumns(); j++) {
-                                obj[2].addtion(i, j, obj[0].give(i, j), obj[1].give(i, j));
-                            }
-                        }
-                        obj[2].printArray(3);
-                    } else {
-                        System.out.println("\nMatrices cannot be added");
-                    }
-                    break;
-                } else if (choice == 2) {
-                    if ((obj[0].getRows() == obj[1].getRows()) && (obj[0].getColumns() == obj[1].getColumns())) {
-                        System.out.println("\nPERFORMING MULTIPLICATION : ");
-                        for (int i = 0; i < obj[0].getRows(); i++) {
-                            for (int j = 0; j < obj[0].getColumns(); j++) {
-                                obj[2].multiplication(i, j, obj[0].give(i, j), obj[1].give(i, j));
-                            }
-                        }
-                        obj[2].printArray(3);
-                    } else {
-                        System.out.println("\nMatrices cannot be multiply");
-                    }
-                    break;
-                } else {
-                    System.out.println("Wrong option selected");
+            do {
+
+                for (int i = 0; i < 2; i++) {
+                    obj[i].printArray(i + 1);
                 }
-            }
-            System.out.println("To play again enter \"yes\" ");
-            play=new Scanner(System.in).next();
-            play=play.toLowerCase();
-        }while(play.equals("yes"));
+
+                while (true) {
+                    System.out.println("ENTER 'add' FOR ADDITION & 'mul' FOR MULTIPLICATION");
+                    String choice = new Scanner(System.in).next();
+                    if (choice.equals("add")) {
+                        if ((obj[0].getRows() == obj[1].getRows()) && (obj[0].getColumns() == obj[1].getColumns())) {
+                            System.out.println("\nPERFORMING ADDITION : ");
+                            for (int i = 0; i < obj[0].getRows(); i++) {
+                                for (int j = 0; j < obj[0].getColumns(); j++) {
+                                    obj[2].addition(i, j, obj[0].give(i, j), obj[1].give(i, j));
+                                }
+                            }
+                            obj[2].printArray(3);
+                        } else {
+                            System.out.println("\nMatrices cannot be added");
+                        }
+                        break;
+                    } else if (choice.equals("mul")) {
+                        if ((obj[0].getRows() == obj[1].getRows()) && (obj[0].getColumns() == obj[1].getColumns())) {
+                            System.out.println("\nPERFORMING MULTIPLICATION : ");
+                            for (int i = 0; i < obj[0].getRows(); i++) {
+                                for (int j = 0; j < obj[0].getColumns(); j++) {
+                                    obj[2].multiplication(i, j, obj[0].give(i, j), obj[1].give(i, j));
+                                }
+                            }
+                            obj[2].printArray(3);
+                        } else {
+                            System.out.println("\nMatrices cannot be multiply");
+                        }
+                        break;
+                    } else {
+                        System.out.println("WRONG OPTION SELECTED  ");
+                    }
+                }
+                while(true)
+                {
+                    System.out.println("*** Do you wish to Play again \"yes\" or \"no\" *** ");
+                    play = new Scanner(System.in).next();
+                    play = play.toLowerCase();
+                    if(play.equals("yes"))
+                    {
+                        break;
+                    }
+                    else if(play.equals("no"))
+                    {
+                        break;
+                    }
+                    else {
+                        System.out.println("WRONG OPTION SELECTED   ");
+                    }
+
+                }
+
+                if (play.equals("yes")) {
+                    while (true) {
+                        System.out.println("***Do you wish to Play with Same array \"yes\" or \"no\" ***  ");
+                        tab = new Scanner(System.in).next();
+                        tab = tab.toLowerCase();
+
+                        if (tab.equals("yes")) {
+                            System.out.println("PLAYING WITH EXISTING ARRAY");
+                            break;
+
+                        } else if (tab.equals("no")) {
+                            System.out.println("CREATING NEW ARRAYS ");
+                            break;
+
+                        } else {
+                            System.out.println("WRONG OPTION SELECTED");
+                        }
+                    }
+                }
+                else {
+                    tab="no";
+                }
+
+            } while (tab.equals("yes"));
+        } while (play.equals("yes"));
     }
 }
 
